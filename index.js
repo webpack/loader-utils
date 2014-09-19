455b0cc23404596304ec06ef10b2268b1c1c6b04
Nick Dima <nick.dima@gmail.com>
2014-09-20 01:25:02 +0200

    Add interpolateName function and documentation

--- index.js
+++ index.js
@@ -1,4 +1,37 @@
 var JSON5 = require("json5");
+var path = require("path");
+
+var baseEncodeTables = {
+	26: "abcdefghijklmnopqrstuvwxyz",
+	32: "123456789abcdefghjkmnpqrstuvwxyz", // no 0lio
+	36: "0123456789abcdefghijklmnopqrstuvwxyz",
+	49: "abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ", // no lIO
+	52: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+	58: "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ", // no 0lIO
+	62: "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+	64: "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_"
+};
+
+function encodeBufferToBase(buffer, base, length) {
+	var encodeTable = baseEncodeTables[base];
+	if (!encodeTable) throw new Error("Enknown encoding base" + base);
+
+	var readLength = buffer.length;
+
+	var b = require('bignum')(0);
+	for (var i = readLength - 1; i >= 0; i--) {
+		b = b.mul(256).add(buffer[i]);
+	}
+
+	var output = "";
+	while (b.gt(0)) {
+		output = encodeTable[b.mod(base)] + output;
+		b = b.div(base);
+	}
+
+	return output;
+}
+
 exports.parseQuery = function parseQuery(query) {
 	if(!query) return {};
 	if(typeof query !== "string")
@@ -113,3 +146,64 @@ exports.parseString = function parseString(str) {
 	}
 	return JSON.parse('"' + str + '"');
 };
+
+exports.interpolateName = function interpolateName(loaderContext, content) {
+	var query = exports.parseQuery(loaderContext.query);
+	var filename = query.name || "[hash].[ext]";
+	var context = query.context || loaderContext.options.context;
+	var ext = "bin";
+	var basename = "file";
+	var directory = "";
+	if(loaderContext.resourcePath) {
+		var resourcePath = loaderContext.resourcePath;
+		var idx = resourcePath.lastIndexOf(".");
+		var i = resourcePath.lastIndexOf("\\");
+		var j = resourcePath.lastIndexOf("/");
+		var p = i < 0 ? j : j < 0 ? i : i < j ? i : j;
+		if(idx >= 0) {
+			ext = resourcePath.substr(idx+1);
+			resourcePath = resourcePath.substr(0, idx);
+		}
+		if(p >= 0) {
+			basename = resourcePath.substr(p+1);
+			resourcePath = resourcePath.substr(0, p+1);
+		}
+		directory = path.relative(context, resourcePath + "_").replace(/\\/g, "/").replace(/\.\.(\/)?/g, "_$1");
+		directory = directory.substr(0, directory.length-1);
+		if(directory.length === 1) directory = "";
+	}
+	var url = filename;
+	if(content) {
+		url = url.replace(/\[hash\]/ig, function() {
+			var digest = query.hash || "md5";
+			var digestSize = query.size || 9999;
+			hash = new (require("crypto").Hash)(digest);
+			hash.update(content);
+			if (query.digest === "base26" || query.digest === "base32" || query.digest === "base36" ||
+			    query.digest === "base49" || query.digest === "base52" || query.digest === "base58" ||
+			    query.digest === "base62" || query.digest === "base64") {
+				return encodeBufferToBase(hash.digest(), query.digest.substr(4), digestSize).substr(0, digestSize);
+			} else {
+				return hash.digest(query.digest || "hex").substr(0, digestSize);
+			}
+		})		
+	}
+	url = url.replace(/\[ext\]/ig, function() {
+		return ext;
+	}).replace(/\[name\]/ig, function() {
+		return basename;
+	}).replace(/\[path\]/ig, function() {
+		return directory;
+	});
+	if(query.regExp && loaderContext.resourcePath) {
+		var re = new RegExp(query.regExp);
+		var match = loaderContext.resourcePath.match(query.regExp);
+		if(match) {
+			for (var i = 1; i < match.length; i++) {
+				var re = new RegExp("\\[" + i + "\\]", "ig");
+				url = url.replace(re, match[i]);
+			}			
+		}
+	}
+	return url;
+}
