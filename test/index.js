9f0949f186be5aca1f2b81e67ad6186a20bf2d69
Tobias Koppers <tobias.koppers@googlemail.com>
2015-05-21 23:33:31 +0200

    added getHashDigest test

--- test/index.js
+++ test/index.js
@@ -73,4 +73,21 @@ describe("loader-utils", function() {
 			});
 		});
 	});
+
+	describe("#getHashDigest", function() {
+		[
+			["test string", "md5", "hex", undefined, "6f8db599de986fab7a21625b7916589c"],
+			["test string", "md5", "hex", 4, "6f8d"],
+			["test string", "md5", "base64", undefined, "2sm1pVmS8xuGJLCdWpJoRL"],
+			["test string", "md5", "base52", undefined, "dJnldHSAutqUacjgfBQGLQx"],
+			["test string", "md5", "base26", 6, "bhtsgu"],
+			["test string", "sha512", "base64", undefined, "2IS-kbfIPnVflXb9CzgoNESGCkvkb0urMmucPD9z8q6HuYz8RShY1-tzSUpm5-Ivx_u4H1MEzPgAhyhaZ7RKog"],
+			["test_string", "md5", "hex", undefined, "3474851a3410906697ec77337df7aae4"]
+		].forEach(function(test) {
+			it("should getHashDigest " + test[0] + " " + test[1] + " " + test[2] + " " + test[3], function() {
+				var hashDigest = loaderUtils.getHashDigest(test[0], test[1], test[2], test[3]);
+				assert.equal(hashDigest, test[4]);
+			});
+		});
+	});
 });
