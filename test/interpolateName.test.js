621114ec5fd40bbbc185f3fd25f0020073f91286
Johannes Ewald <johannes.ewald@peerigon.com>
2017-02-17 17:49:08 +0100

    Move tests into dedicated files

---
+++ test/interpolateName.test.js
@@ -0,0 +1,99 @@
+"use strict";
+
+const assert = require("assert");
+const loaderUtils = require("../");
+
+const emojiRegex = /[\uD800-\uDFFF]./;
+
+describe("interpolateName()", () => {
+	function run(tests) {
+		tests.forEach((test) => {
+			const args = test[0];
+			const expected = test[1];
+			const message = test[2];
+			it(message, () => {
+				const result = loaderUtils.interpolateName.apply(loaderUtils, args);
+				if(typeof expected === "function") {
+					expected(result);
+				} else {
+					assert.equal(result, expected);
+				}
+			});
+		});
+	}
+
+	[
+		["/app/js/javascript.js", "js/[hash].script.[ext]", "test content", "js/9473fdd0d880a43c21b7778d34872157.script.js"],
+		["/app/page.html", "html-[hash:6].html", "test content", "html-9473fd.html"],
+		["/app/flash.txt", "[hash]", "test content", "9473fdd0d880a43c21b7778d34872157"],
+		["/app/img/image.png", "[sha512:hash:base64:7].[ext]", "test content", "2BKDTjl.png"],
+		["/app/dir/file.png", "[path][name].[ext]?[hash]", "test content", "/app/dir/file.png?9473fdd0d880a43c21b7778d34872157"],
+		["/vendor/test/images/loading.gif", path => path.replace(/\/?vendor\/?/, ""), "test content", "test/images/loading.gif"]
+	].forEach(test => {
+		it("should interpolate " + test[0] + " " + test[1], () => {
+			const interpolatedName = loaderUtils.interpolateName({ resourcePath: test[0] }, test[1], { content: test[2] });
+			assert.equal(interpolatedName, test[3]);
+		});
+	});
+
+	run([
+		[[{}, "", { content: "test string" }], "6f8db599de986fab7a21625b7916589c.bin", "should interpolate default tokens"],
+		[[{}, "[hash:base64]", { content: "test string" }], "2sm1pVmS8xuGJLCdWpJoRL", "should interpolate [hash] token with options"],
+		[[{}, "[unrecognized]", { content: "test string" }], "[unrecognized]", "should not interpolate unrecognized token"],
+		[
+			[{}, "[emoji]", { content: "test" }],
+			result => {
+				assert.ok(emojiRegex.test(result), result);
+			},
+			"should interpolate [emoji]"
+		],
+		[
+			[{}, "[emoji:3]", { content: "string" }],
+			result => {
+				assert.ok(emojiRegex.test(result), result);
+				assert.ok(result.length, 6);
+			},
+			"should interpolate [emoji:3]"
+		],
+	]);
+
+	it("should return the same emoji for the same string", () => {
+		const args = [{}, "[emoji:5]", { content: "same_emoji" }];
+		const result1 = loaderUtils.interpolateName.apply(loaderUtils, args);
+		const result2 = loaderUtils.interpolateName.apply(loaderUtils, args);
+		assert.equal(result1, result2);
+	});
+
+	context("no loader context", () => {
+		const loaderContext = {};
+		run([
+			[[loaderContext, "[ext]", {}], "bin", "should interpolate [ext] token"],
+			[[loaderContext, "[name]", {}], "file", "should interpolate [name] token"],
+			[[loaderContext, "[path]", {}], "", "should interpolate [path] token"],
+			[[loaderContext, "[folder]", {}], "", "should interpolate [folder] token"]
+		]);
+	});
+
+	context("with loader context", () => {
+		const loaderContext = { resourcePath: "/path/to/file.exe" };
+		run([
+			[[loaderContext, "[ext]", {}], "exe", "should interpolate [ext] token"],
+			[[loaderContext, "[name]", {}], "file", "should interpolate [name] token"],
+			[[loaderContext, "[path]", {}], "/path/to/", "should interpolate [path] token"],
+			[[loaderContext, "[folder]", {}], "to", "should interpolate [folder] token"]
+		]);
+	});
+
+	run([
+		[[{
+			resourcePath: "/xyz",
+			options: {
+				customInterpolateName(str, name, options) {
+					return str + "-" + name + "-" + options.special;
+				}
+			}
+		}, "[name]", {
+			special: "special"
+		}], "xyz-[name]-special", "should provide a custom interpolateName function in options"],
+	]);
+});
---
+++ test/urlToRequest.test.js
@@ -0,0 +1,54 @@
+"use strict";
+
+const assert = require("assert");
+const loaderUtils = require("../");
+
+function ExpectedError(regex) {
+	this.regex = regex;
+}
+ExpectedError.prototype.matches = function(err) {
+	return this.regex.test(err.message);
+};
+
+describe("urlToRequest()", () => {
+	[
+		// without root
+		[["path/to/thing"], "./path/to/thing", "should handle implicit relative urls"],
+		[["./path/to/thing"], "./path/to/thing", "should handle explicit relative urls"],
+		[["~path/to/thing"], "path/to/thing", "should handle module urls (with ~)"],
+		[["some/other/stuff/and/then~path/to/thing"], "path/to/thing", "should handle module urls with path prefix"],
+		[["./some/other/stuff/and/then~path/to/thing"], "path/to/thing", "should handle module urls with relative path prefix"],
+		// with root (normal path)
+		[["path/to/thing", "root/dir"], "./path/to/thing", "should do nothing with root if implicit relative url"],
+		[["./path/to/thing", "root/dir"], "./path/to/thing", "should do nothing with root if explicit relative url"],
+		[["/path/to/thing", "root/dir"], "root/dir/path/to/thing", "should include root if root-relative url"],
+		// with root (boolean)
+		[["/path/to/thing", true], "/path/to/thing", "should allow root-relative to exist as-is if root = `true`"],
+		// with root (boolean) on Windows
+		[["C:\\path\\to\\thing"], "C:\\path\\to\\thing", "should handle Windows absolute paths with drive letter"],
+		[["\\\\?\\UNC\\ComputerName\\path\\to\\thing"], "\\\\?\\UNC\\ComputerName\\path\\to\\thing", "should handle Windows absolute UNC paths"],
+		// with root (module)
+		[["/path/to/thing", "~"], "path/to/thing", "should convert to module url if root = ~"],
+		// with root (module path)
+		[["/path/to/thing", "~module"], "module/path/to/thing", "should allow module prefixes when root starts with ~"],
+		[["/path/to/thing", "~module/"], "module/path/to/thing", "should allow module prefixes (with trailing slash) when root starts with ~"],
+		// error cases
+		[["/path/to/thing", 1], new ExpectedError(/unexpected parameters/i), "should throw an error on invalid root"],
+		// difficult cases
+		[["a:b-not-\\window-path"], "./a:b-not-\\window-path", "should not incorrectly detect windows paths"]
+	].forEach((test) => {
+		it(test[2], () => {
+			const expected = test[1];
+			try {
+				const request = loaderUtils.urlToRequest.apply(loaderUtils, test[0]);
+				assert.equal(request, expected);
+			} catch(e) {
+				if(expected instanceof ExpectedError) {
+					assert.ok(expected.matches(e));
+				} else {
+					assert.ok(false, "should not have thrown an error: " + e.message);
+				}
+			}
+		});
+	});
+});
