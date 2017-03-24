fc57e990c03b13984e4f8b14753c61938bb1a391
David Barri <japgolly@gmail.com>
2017-03-24 13:35:32 +1100

    PR feedback: support http, https, and //

--- test/urlToRequest.test.js
+++ test/urlToRequest.test.js
@@ -13,7 +13,9 @@ ExpectedError.prototype.matches = function(err) {
 describe("urlToRequest()", () => {
 	[
 		// without root
-		[["https://google.com"], "https://google.com", "should handle absolute urls"],
+		[["//google.com"], "//google.com", "should handle scheme-agnostic urls"],
+		[["http://google.com"], "http://google.com", "should handle http urls"],
+		[["https://google.com"], "https://google.com", "should handle https urls"],
 		[["path/to/thing"], "./path/to/thing", "should handle implicit relative urls"],
 		[["./path/to/thing"], "./path/to/thing", "should handle explicit relative urls"],
 		[["~path/to/thing"], "path/to/thing", "should handle module urls (with ~)"],
