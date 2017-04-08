990e38ff6508d3336322b4af8a9e0d9820e86a40
kof <215098326@qq.com>
2017-04-08 15:45:42 +0800

    Update isUrlRequest.test.js

--- test/isUrlRequest.test.js
+++ test/isUrlRequest.test.js
@@ -8,10 +8,9 @@ describe("isUrlRequest()", () => {
         ["about:blank", undefined, false],
     ].forEach(test => {
         const expected = test.pop();
-
         it("should return " + expected + " when called with " + test.join(), () => {
-            const urlRequest = loaderUtils.isUrlRequest(test[0], test[1]);
-            assert.equal(urlRequest, expected);
-        })
+			const urlRequest = loaderUtils.isUrlRequest(test[0], test[1]);
+			assert.equal(urlRequest, expected);
+        });
     });
 });
