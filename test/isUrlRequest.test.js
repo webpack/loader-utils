4ebf4c46727c6e19e348b60d47fe7830345984dd
kof <215098326@qq.com>
2017-04-08 15:36:48 +0800

    Update isUrlRequest.test.js
    To improve the isUrlRequest.test.js output

--- test/isUrlRequest.test.js
+++ test/isUrlRequest.test.js
@@ -4,12 +4,14 @@ const assert = require("assert");
 const loaderUtils = require("../");
 
 describe("isUrlRequest()", () => {
-	[
-		["test url", "about:blank", undefined, false],
-	].forEach(test => {
-		it("should isUrlRequest " + test[0] + " " + test[1] + " " + test[2], () => {
-			const urlRequest = loaderUtils.isUrlRequest(test[1], test[2]);
-			assert.equal(urlRequest, test[3]);
-		});
-	});
+    [
+        ["about:blank", undefined, false],
+    ].forEach(test => {
+        const expected = test.pop();
+
+        it("should return " + expected + " when called with " + test.join(), () => {
+            const urlRequest = loaderUtils.isUrlRequest(test[0], test[1]);
+            assert.equal(urlRequest, expected);
+        })
+    });
 });
