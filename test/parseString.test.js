97cb868d102e5494cb79d4314d2ecac05fec56bd
evilebottnawi <sheo13666q@gmail.com>
2018-12-24 18:42:06 +0300

    style: integrate prettier

--- test/parseString.test.js
+++ test/parseString.test.js
@@ -1,19 +1,22 @@
-"use strict";
+'use strict';
 
-const assert = require("assert");
-const loaderUtils = require("../");
+const assert = require('assert');
+const loaderUtils = require('../');
 
-describe("parseString()", () => {
-	[
-		["test string", "test string"],
-		[JSON.stringify("!\"§$%&/()=?'*#+,.-;öäü:_test"), "!\"§$%&/()=?'*#+,.-;öäü:_test"],
-		["'escaped with single \"'", "escaped with single \""],
-		["invalid \"' string", "invalid \"' string"],
-		["'inconsistent start and end\"", "'inconsistent start and end\""]
-	].forEach(test => {
-		it("should parse " + test[0], () => {
-			const parsed = loaderUtils.parseString(test[0]);
-			assert.equal(parsed, test[1]);
-		});
-	});
+describe('parseString()', () => {
+  [
+    ['test string', 'test string'],
+    [
+      JSON.stringify('!"§$%&/()=?\'*#+,.-;öäü:_test'),
+      '!"§$%&/()=?\'*#+,.-;öäü:_test',
+    ],
+    ["'escaped with single \"'", 'escaped with single "'],
+    ['invalid "\' string', 'invalid "\' string'],
+    ['\'inconsistent start and end"', '\'inconsistent start and end"'],
+  ].forEach((test) => {
+    it('should parse ' + test[0], () => {
+      const parsed = loaderUtils.parseString(test[0]);
+      assert.equal(parsed, test[1]);
+    });
+  });
 });
