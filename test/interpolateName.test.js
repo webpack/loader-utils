8983452ec96e151bd4d127fc031060614f50c60d
evilebottnawi <sheo13666q@gmail.com>
2021-10-20 13:54:28 +0300

    chore(deps): update

--- test/interpolateName.test.js
+++ test/interpolateName.test.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
-const loaderUtils = require('../');
+const loaderUtils = require("../");
 
 const emojiRegex = /[\uD800-\uDFFF]./;
 
-describe('interpolateName()', () => {
+describe("interpolateName()", () => {
   function run(tests) {
     tests.forEach((test) => {
       const args = test[0];
@@ -12,7 +12,7 @@ describe('interpolateName()', () => {
       const message = test[2];
       it(message, () => {
         const result = loaderUtils.interpolateName.apply(loaderUtils, args);
-        if (typeof expected === 'function') {
+        if (typeof expected === "function") {
           expected(result);
         } else {
           expect(result).toBe(expected);
@@ -23,200 +23,200 @@ describe('interpolateName()', () => {
 
   [
     [
-      '/app/js/javascript.js',
-      'js/[hash].script.[ext]',
-      'test content',
-      'js/a69899814931280e2f527219ad6ac754.script.js',
+      "/app/js/javascript.js",
+      "js/[hash].script.[ext]",
+      "test content",
+      "js/a69899814931280e2f527219ad6ac754.script.js",
     ],
     [
-      '/app/js/javascript.js',
-      'js/[contenthash].script.[ext]',
-      'test content',
-      'js/a69899814931280e2f527219ad6ac754.script.js',
+      "/app/js/javascript.js",
+      "js/[contenthash].script.[ext]",
+      "test content",
+      "js/a69899814931280e2f527219ad6ac754.script.js",
     ],
     [
-      '/app/page.html',
-      'html-[hash:6].html',
-      'test content',
-      'html-a69899.html',
+      "/app/page.html",
+      "html-[hash:6].html",
+      "test content",
+      "html-a69899.html",
     ],
     [
-      '/app/page.html',
-      'html-[contenthash:6].html',
-      'test content',
-      'html-a69899.html',
+      "/app/page.html",
+      "html-[contenthash:6].html",
+      "test content",
+      "html-a69899.html",
     ],
     [
-      '/app/flash.txt',
-      '[hash]',
-      'test content',
-      'a69899814931280e2f527219ad6ac754',
+      "/app/flash.txt",
+      "[hash]",
+      "test content",
+      "a69899814931280e2f527219ad6ac754",
     ],
     [
-      '/app/flash.txt',
-      '[contenthash]',
-      'test content',
-      'a69899814931280e2f527219ad6ac754',
+      "/app/flash.txt",
+      "[contenthash]",
+      "test content",
+      "a69899814931280e2f527219ad6ac754",
     ],
     [
-      '/app/img/image.png',
-      '[sha512:hash:base64:7].[ext]',
-      'test content',
-      '2BKDTjl.png',
+      "/app/img/image.png",
+      "[sha512:hash:base64:7].[ext]",
+      "test content",
+      "2BKDTjl.png",
     ],
     [
-      '/app/img/image.png',
-      '[sha512:contenthash:base64:7].[ext]',
-      'test content',
-      '2BKDTjl.png',
+      "/app/img/image.png",
+      "[sha512:contenthash:base64:7].[ext]",
+      "test content",
+      "2BKDTjl.png",
     ],
     [
-      '/app/dir/file.png',
-      '[path][name].[ext]?[hash]',
-      'test content',
-      '/app/dir/file.png?a69899814931280e2f527219ad6ac754',
+      "/app/dir/file.png",
+      "[path][name].[ext]?[hash]",
+      "test content",
+      "/app/dir/file.png?a69899814931280e2f527219ad6ac754",
     ],
     [
-      '/app/dir/file.png',
-      '[path][name].[ext]?[contenthash]',
-      'test content',
-      '/app/dir/file.png?a69899814931280e2f527219ad6ac754',
+      "/app/dir/file.png",
+      "[path][name].[ext]?[contenthash]",
+      "test content",
+      "/app/dir/file.png?a69899814931280e2f527219ad6ac754",
     ],
     [
-      '/vendor/test/images/loading.gif',
-      (path) => path.replace(/\/?vendor\/?/, ''),
-      'test content',
-      'test/images/loading.gif',
+      "/vendor/test/images/loading.gif",
+      (path) => path.replace(/\/?vendor\/?/, ""),
+      "test content",
+      "test/images/loading.gif",
     ],
     [
-      '/pathWith.period/filename.js',
-      'js/[name].[ext]',
-      'test content',
-      'js/filename.js',
+      "/pathWith.period/filename.js",
+      "js/[name].[ext]",
+      "test content",
+      "js/filename.js",
     ],
     [
-      '/pathWith.period/filenameWithoutExt',
-      'js/[name].[ext]',
-      'test content',
-      'js/filenameWithoutExt.bin',
+      "/pathWith.period/filenameWithoutExt",
+      "js/[name].[ext]",
+      "test content",
+      "js/filenameWithoutExt.bin",
     ],
     [
-      '/lib/components/modal/modal.css',
-      '[name]__modalTitle___[sha1:hash:hex:4]',
-      'test content',
-      'modal__modalTitle___1eeb',
+      "/lib/components/modal/modal.css",
+      "[name]__modalTitle___[sha1:hash:hex:4]",
+      "test content",
+      "modal__modalTitle___1eeb",
     ],
     [
-      '/lib/components/modal/modal.css',
-      '[name]__modalTitle___[sha1:contenthash:hex:4]',
-      'test content',
-      'modal__modalTitle___1eeb',
+      "/lib/components/modal/modal.css",
+      "[name]__modalTitle___[sha1:contenthash:hex:4]",
+      "test content",
+      "modal__modalTitle___1eeb",
     ],
     [
-      '/lib/components/modal/modal.css',
-      '[name].[md5:hash:base64:20].[ext]',
-      'test content',
-      'modal.1n8osQznuT8jOAwdzg_n.css',
+      "/lib/components/modal/modal.css",
+      "[name].[md5:hash:base64:20].[ext]",
+      "test content",
+      "modal.1n8osQznuT8jOAwdzg_n.css",
     ],
     [
-      '/lib/components/modal/modal.css',
-      '[name].[md5:contenthash:base64:20].[ext]',
-      'test content',
-      'modal.1n8osQznuT8jOAwdzg_n.css',
+      "/lib/components/modal/modal.css",
+      "[name].[md5:contenthash:base64:20].[ext]",
+      "test content",
+      "modal.1n8osQznuT8jOAwdzg_n.css",
     ],
     // Should not interpret without `hash` or `contenthash`
     [
-      '/lib/components/modal/modal.css',
-      '[name].[md5::base64:20].[ext]',
-      'test content',
-      'modal.[md5::base64:20].css',
+      "/lib/components/modal/modal.css",
+      "[name].[md5::base64:20].[ext]",
+      "test content",
+      "modal.[md5::base64:20].css",
     ],
     [
-      '/app/js/javascript.js?foo=bar',
-      'js/[hash].script.[ext][query]',
-      'test content',
-      'js/a69899814931280e2f527219ad6ac754.script.js?foo=bar',
+      "/app/js/javascript.js?foo=bar",
+      "js/[hash].script.[ext][query]",
+      "test content",
+      "js/a69899814931280e2f527219ad6ac754.script.js?foo=bar",
     ],
     [
-      '/app/js/javascript.js?foo=bar&bar=baz',
-      'js/[hash].script.[ext][query]',
-      'test content',
-      'js/a69899814931280e2f527219ad6ac754.script.js?foo=bar&bar=baz',
+      "/app/js/javascript.js?foo=bar&bar=baz",
+      "js/[hash].script.[ext][query]",
+      "test content",
+      "js/a69899814931280e2f527219ad6ac754.script.js?foo=bar&bar=baz",
     ],
     [
-      '/app/js/javascript.js?foo',
-      'js/[hash].script.[ext][query]',
-      'test content',
-      'js/a69899814931280e2f527219ad6ac754.script.js?foo',
+      "/app/js/javascript.js?foo",
+      "js/[hash].script.[ext][query]",
+      "test content",
+      "js/a69899814931280e2f527219ad6ac754.script.js?foo",
     ],
     [
-      '/app/js/javascript.js?',
-      'js/[hash].script.[ext][query]',
-      'test content',
-      'js/a69899814931280e2f527219ad6ac754.script.js',
+      "/app/js/javascript.js?",
+      "js/[hash].script.[ext][query]",
+      "test content",
+      "js/a69899814931280e2f527219ad6ac754.script.js",
     ],
     [
-      '/app/js/javascript.js?a',
-      'js/[hash].script.[ext][query]',
-      'test content',
-      'js/a69899814931280e2f527219ad6ac754.script.js?a',
+      "/app/js/javascript.js?a",
+      "js/[hash].script.[ext][query]",
+      "test content",
+      "js/a69899814931280e2f527219ad6ac754.script.js?a",
     ],
     [
-      '/app/js/javascript.js?foo=bar#hash',
-      'js/[hash].script.[ext][query]',
-      'test content',
-      'js/a69899814931280e2f527219ad6ac754.script.js?foo=bar',
+      "/app/js/javascript.js?foo=bar#hash",
+      "js/[hash].script.[ext][query]",
+      "test content",
+      "js/a69899814931280e2f527219ad6ac754.script.js?foo=bar",
     ],
     [
-      '/app/js/javascript.js?foo=bar#hash',
+      "/app/js/javascript.js?foo=bar#hash",
       (resourcePath, resourceQuery) => {
         expect(resourcePath).toBeDefined();
         expect(resourceQuery).toBeDefined();
 
-        return 'js/[hash].script.[ext][query]';
+        return "js/[hash].script.[ext][query]";
       },
-      'test content',
-      'js/a69899814931280e2f527219ad6ac754.script.js?foo=bar',
+      "test content",
+      "js/a69899814931280e2f527219ad6ac754.script.js?foo=bar",
     ],
     [
-      '/app/js/javascript.js?a',
+      "/app/js/javascript.js?a",
       (resourcePath, resourceQuery) => {
         expect(resourcePath).toBeDefined();
         expect(resourceQuery).toBeDefined();
 
-        return 'js/[hash].script.[ext][query]';
+        return "js/[hash].script.[ext][query]";
       },
-      'test content',
-      'js/a69899814931280e2f527219ad6ac754.script.js?a',
+      "test content",
+      "js/a69899814931280e2f527219ad6ac754.script.js?a",
     ],
     [
-      '/app/js/javascript.js',
+      "/app/js/javascript.js",
       (resourcePath, resourceQuery) => {
         expect(resourcePath).toBeDefined();
         expect(resourceQuery).not.toBeDefined();
 
-        return 'js/[hash].script.[ext][query]';
+        return "js/[hash].script.[ext][query]";
       },
-      'test content',
-      'js/a69899814931280e2f527219ad6ac754.script.js',
+      "test content",
+      "js/a69899814931280e2f527219ad6ac754.script.js",
     ],
     [
-      '/app/js/javascript.js?',
+      "/app/js/javascript.js?",
       (resourcePath, resourceQuery) => {
         expect(resourcePath).toBeDefined();
         expect(resourceQuery).not.toBeDefined();
 
-        return 'js/[hash].script.[ext][query]';
+        return "js/[hash].script.[ext][query]";
       },
-      'test content',
-      'js/a69899814931280e2f527219ad6ac754.script.js',
+      "test content",
+      "js/a69899814931280e2f527219ad6ac754.script.js",
     ],
   ].forEach((test) => {
-    it('should interpolate ' + test[0] + ' ' + test[1], () => {
-      let resourcePath = '';
-      let resourceQuery = '';
+    it("should interpolate " + test[0] + " " + test[1], () => {
+      let resourcePath = "";
+      let resourceQuery = "";
 
-      const queryIdx = test[0].indexOf('?');
+      const queryIdx = test[0].indexOf("?");
 
       if (queryIdx >= 0) {
         resourcePath = test[0].substr(0, queryIdx);
@@ -236,114 +236,114 @@ describe('interpolateName()', () => {
   });
 
   [
-    'sha1fakename',
-    '9dxfakename',
-    'RSA-SHA256-fakename',
-    'ecdsa-with-SHA1-fakename',
-    'tls1.1-sha512-fakename',
+    "sha1fakename",
+    "9dxfakename",
+    "RSA-SHA256-fakename",
+    "ecdsa-with-SHA1-fakename",
+    "tls1.1-sha512-fakename",
   ].forEach((hashName) => {
-    it('should pick hash algorithm by name ' + hashName, () => {
+    it("should pick hash algorithm by name " + hashName, () => {
       expect(() => {
         const interpolatedName = loaderUtils.interpolateName(
           {},
-          '[' + hashName + ':hash:base64:10]',
-          { content: 'a' }
+          "[" + hashName + ":hash:base64:10]",
+          { content: "a" }
         );
         // if for any reason the system we're running on has a hash
         // algorithm matching any of our bogus names, at least make sure
         // the output is not the unmodified name:
-        expect(interpolatedName[0]).not.toBe('[');
+        expect(interpolatedName[0]).not.toBe("[");
       }).toThrow(/digest method not supported/i);
     });
   });
 
   run([
     [
-      [{}, '', { content: 'test string' }],
-      '2e06edd4f1623268c5a51730d8a0b2af.bin',
-      'should interpolate default tokens',
+      [{}, "", { content: "test string" }],
+      "2e06edd4f1623268c5a51730d8a0b2af.bin",
+      "should interpolate default tokens",
     ],
     [
-      [{}, '[hash:base64]', { content: 'test string' }],
-      '2LIG3oc1uBNmwOoL7kXgoK',
-      'should interpolate [hash] token with options',
+      [{}, "[hash:base64]", { content: "test string" }],
+      "2LIG3oc1uBNmwOoL7kXgoK",
+      "should interpolate [hash] token with options",
     ],
     [
-      [{}, '[unrecognized]', { content: 'test string' }],
-      '[unrecognized]',
-      'should not interpolate unrecognized token',
+      [{}, "[unrecognized]", { content: "test string" }],
+      "[unrecognized]",
+      "should not interpolate unrecognized token",
     ],
     [
-      [{}, '[emoji]', { content: 'test' }],
+      [{}, "[emoji]", { content: "test" }],
       (result) => {
         expect(emojiRegex.test(result)).toBe(true);
       },
-      'should interpolate [emoji]',
+      "should interpolate [emoji]",
     ],
     [
-      [{}, '[emoji:3]', { content: 'string' }],
+      [{}, "[emoji:3]", { content: "string" }],
       (result) => {
         expect(emojiRegex.test(result)).toBe(true);
         expect(result.length).toBeDefined();
       },
-      'should interpolate [emoji:3]',
+      "should interpolate [emoji:3]",
     ],
   ]);
 
-  it('should return the same emoji for the same string', () => {
-    const args = [{}, '[emoji:5]', { content: 'same_emoji' }];
+  it("should return the same emoji for the same string", () => {
+    const args = [{}, "[emoji:5]", { content: "same_emoji" }];
     const result1 = loaderUtils.interpolateName.apply(loaderUtils, args);
     const result2 = loaderUtils.interpolateName.apply(loaderUtils, args);
 
     expect(result1).toBe(result2);
   });
 
-  it('should throw error when out of emoji', () => {
+  it("should throw error when out of emoji", () => {
     expect(() => {
       loaderUtils.interpolateName.apply(loaderUtils, [
         {},
-        '[emoji:5000]',
-        { content: 'foo' },
+        "[emoji:5000]",
+        { content: "foo" },
       ]);
-    }).toThrow('Ran out of emoji');
+    }).toThrow("Ran out of emoji");
   });
 
-  describe('no loader context', () => {
+  describe("no loader context", () => {
     const loaderContext = {};
     run([
-      [[loaderContext, '[ext]', {}], 'bin', 'should interpolate [ext] token'],
+      [[loaderContext, "[ext]", {}], "bin", "should interpolate [ext] token"],
       [
-        [loaderContext, '[name]', {}],
-        'file',
-        'should interpolate [name] token',
+        [loaderContext, "[name]", {}],
+        "file",
+        "should interpolate [name] token",
       ],
-      [[loaderContext, '[path]', {}], '', 'should interpolate [path] token'],
+      [[loaderContext, "[path]", {}], "", "should interpolate [path] token"],
       [
-        [loaderContext, '[folder]', {}],
-        '',
-        'should interpolate [folder] token',
+        [loaderContext, "[folder]", {}],
+        "",
+        "should interpolate [folder] token",
       ],
     ]);
   });
 
-  describe('with loader context', () => {
-    const loaderContext = { resourcePath: '/path/to/file.exe' };
+  describe("with loader context", () => {
+    const loaderContext = { resourcePath: "/path/to/file.exe" };
     run([
-      [[loaderContext, '[ext]', {}], 'exe', 'should interpolate [ext] token'],
+      [[loaderContext, "[ext]", {}], "exe", "should interpolate [ext] token"],
       [
-        [loaderContext, '[name]', {}],
-        'file',
-        'should interpolate [name] token',
+        [loaderContext, "[name]", {}],
+        "file",
+        "should interpolate [name] token",
       ],
       [
-        [loaderContext, '[path]', {}],
-        '/path/to/',
-        'should interpolate [path] token',
+        [loaderContext, "[path]", {}],
+        "/path/to/",
+        "should interpolate [path] token",
       ],
       [
-        [loaderContext, '[folder]', {}],
-        'to',
-        'should interpolate [folder] token',
+        [loaderContext, "[folder]", {}],
+        "to",
+        "should interpolate [folder] token",
       ],
     ]);
   });
@@ -352,33 +352,33 @@ describe('interpolateName()', () => {
     [
       [
         {
-          resourcePath: '/xyz',
+          resourcePath: "/xyz",
           options: {
             customInterpolateName(str, name, options) {
-              return str + '-' + name + '-' + options.special;
+              return str + "-" + name + "-" + options.special;
             },
           },
         },
-        '[name]',
+        "[name]",
         {
-          special: 'special',
+          special: "special",
         },
       ],
-      'xyz-[name]-special',
-      'should provide a custom interpolateName function in options',
+      "xyz-[name]-special",
+      "should provide a custom interpolateName function in options",
     ],
     [
       [
         {
-          resourcePath: '/foo/xyz.png',
+          resourcePath: "/foo/xyz.png",
         },
-        '[1]-[name].[ext]',
+        "[1]-[name].[ext]",
         {
           regExp: /\/([a-z0-9]+)\/[a-z0-9]+\.png$/,
         },
       ],
-      'foo-xyz.png',
-      'should support regExp in options',
+      "foo-xyz.png",
+      "should support regExp in options",
     ],
   ]);
 });
--- test/parseString.test.js
+++ test/parseString.test.js
@@ -1,19 +1,19 @@
-'use strict';
+"use strict";
 
-const loaderUtils = require('../');
+const loaderUtils = require("../");
 
-describe('parseString()', () => {
+describe("parseString()", () => {
   [
-    ['test string', 'test string'],
+    ["test string", "test string"],
     [
-      JSON.stringify('!"§$%&/()=?\'*#+,.-;öäü:_test'),
-      '!"§$%&/()=?\'*#+,.-;öäü:_test',
+      JSON.stringify("!\"§$%&/()=?'*#+,.-;öäü:_test"),
+      "!\"§$%&/()=?'*#+,.-;öäü:_test",
     ],
     ["'escaped with single \"'", 'escaped with single "'],
-    ['invalid "\' string', 'invalid "\' string'],
-    ['\'inconsistent start and end"', '\'inconsistent start and end"'],
+    ["invalid \"' string", "invalid \"' string"],
+    ["'inconsistent start and end\"", "'inconsistent start and end\""],
   ].forEach((test) => {
-    it('should parse ' + test[0], () => {
+    it("should parse " + test[0], () => {
       const parsed = loaderUtils.parseString(test[0]);
 
       expect(parsed).toBe(test[1]);
