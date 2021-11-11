451858b0bb33911d52d2f03a6470fd2b86493b84
Anton Khlynovskiy <subzey@gmail.com>
2021-11-11 18:37:22 +0300

    feat: hash uniformity for base digests

--- test/getHashDigest.test.js
+++ test/getHashDigest.test.js
@@ -11,8 +11,8 @@ describe("getHashDigest()", () => {
     ["abc\\0💩", "xxhash64", "hex", undefined, "86733ec125b93904"],
     ["abc\\0💩", "xxhash64", "base64", undefined, "hnM+wSW5OQQ="],
     ["abc\\0♥", "xxhash64", "base64", undefined, "S5o0KX3APSA="],
-    ["abc\\0💩", "xxhash64", "base52", undefined, "cfByjQcJZIU"],
-    ["abc\\0♥", "xxhash64", "base52", undefined, "qdLyAQjLlod"],
+    ["abc\\0💩", "xxhash64", "base52", undefined, "acfByjQcJZIU"],
+    ["abc\\0♥", "xxhash64", "base52", undefined, "aqdLyAQjLlod"],
 
     ["test string", "md4", "hex", 4, "2e06"],
     ["test string", "md4", "base64", undefined, "Lgbt1PFiMmjFpRcw2KCyrw=="],
@@ -34,7 +34,8 @@ describe("getHashDigest()", () => {
     ],
     ["test string", "md5", "base52", undefined, "dJnldHSAutqUacjgfBQGLQx"],
     ["test string", "md5", "base64", undefined, "b421md6Yb6t6IWJbeRZYnA=="],
-    ["test string", "md5", "base26", 6, "bhtsgu"],
+    ["test string", "md5", "base26", undefined, "bhtsgujtzvmjtgtzlqvubqggbvgx"],
+    ["test string", "md5", "base26", 6, "ggbvgx"],
     ["abc\\0♥", "md5", "hex", undefined, "2e897b64f8050e66aff98d38f7a012c5"],
     ["abc\\0💩", "md5", "hex", undefined, "63ad5b3d675c5890e0c01ed339ba0187"],
     ["abc\\0💩", "md5", "base64", undefined, "Y61bPWdcWJDgwB7TOboBhw=="],
@@ -77,5 +78,48 @@ describe("getHashDigest()", () => {
         expect(hashDigest).toBe(test[4]);
       }
     );
+  });
+});
+
+function testDistribution(digestType, length, tableSize, iterations) {
+  const lowerBound = Math.round(iterations / 2);
+  const upperBound = Math.round(iterations * 2);
+
+  const stats = [];
+  for (let i = tableSize * iterations; i-- > 0; ) {
+    const generatedString = loaderUtils.getHashDigest(
+      `Some input #${i}`,
+      undefined,
+      digestType,
+      length
+    );
+
+    for (let pos = 0; pos < length; pos++) {
+      const char = generatedString[pos];
+      stats[pos] = stats[pos] || {};
+      stats[pos][char] = (stats[pos][char] || 0) + 1;
+    }
+  }
+
+  for (let pos = 0; pos < length; pos++) {
+    const chars = Object.keys(stats[pos]).sort();
+    test(`distinct chars at position ${pos}`, () => {
+      expect(chars.length).toBe(tableSize);
+    });
+    for (const char of chars) {
+      test(`occurences of char "${char}" at position ${pos} should be around ${iterations}`, () => {
+        expect(stats[pos][char]).toBeLessThanOrEqual(upperBound);
+        expect(stats[pos][char]).toBeGreaterThanOrEqual(lowerBound);
+      });
+    }
+  }
+}
+
+describe("getHashDigest() char distribution", () => {
+  describe("should be uniform for base62", () => {
+    testDistribution("base62", 8, 62, 100);
+  });
+  describe("should be uniform for base26", () => {
+    testDistribution("base26", 8, 26, 100);
   });
 });
